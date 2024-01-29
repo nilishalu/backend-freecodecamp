547154ca04d161faf898ef4df91040c8d0c78b25
nilishalu <shalini.c.e.ganu@gmail.com>
2024-01-30 00:48:39 +0530

    add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/home", (req, res) => {
-  res.status(200).json("Hello from Google!");
+  res.send("Hello from Google!");
 });
 
 app.listen(3000, () => {
