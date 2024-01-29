234f12ccd7a5615ee5ee4f2197f643813f692d83
nilishalu <shalini.c.e.ganu@gmail.com>
2024-01-30 00:21:07 +0530

    cleaned up things

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from Google!")
-})
+  res.send("Hello from Google!");
+});
 
 app.listen(3000, () => {
-    console.log("Server running at port 3000")
-})
+  console.log("Hello World");
+});
+
+module.exports = app;
