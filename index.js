c7fcfe7ab30ccaed3663cc482a274d3ffecef5eb
nilishalu <shalini.c.e.ganu@gmail.com>
2024-01-30 00:26:44 +0530

    hope this works

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Google!");
+  res.status(200).json("Hello from Google!");
 });
 
 app.listen(3000, () => {
