d1bebed8f7ceee3bb8d68d130e3511b321a11fa5
nilishalu <shalini.c.e.ganu@gmail.com>
2024-01-29 23:38:33 +0530

    api defined

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello from Google!")
+})
+
 app.listen(3000, () => {
     console.log("Server running at port 3000")
 })
